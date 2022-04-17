6543b13aacc9afea4b72903aece1977feac74d1c
OneS18 <diablo201888@gmail.com>
2022-04-18 00:30:06 +0300

    fix here and there

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,7 @@ const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    throw new BadRequestError("please provide all values");
+    throw new BadRequestError("please provide all valuess");
   }
 
   const userAlreadyExists = await User.findOne({ email });
@@ -17,7 +17,7 @@ const register = async (req, res) => {
   const user = await User.create({ name, email, password });
 
   const token = user.createJWT();
-  res.status(StatusCodes.OK).json({
+  res.status(StatusCodes.CREATED).json({
     user: {
       email: user.email,
       lastName: user.lastName,
@@ -43,9 +43,9 @@ const login = async (req, res) => {
   if (!isPasswordCorrect) {
     throw new UnAuthenticatedError("Invalid Credentials");
   }
-  const token = user.createJWT().json({ user, token, location: user.location });
+  const token = user.createJWT();
   user.password = undefined;
-  res.status(StatusCodes.OK);
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
 const updateUser = async (req, res) => {
   res.send("updateUser");
